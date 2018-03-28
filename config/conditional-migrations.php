2dcf404caba84bd9c7dc296d312bc2e600b6732e
Sven Luijten <sven.luijten@medpets.nl>
2018-03-28 15:30:46 +0200

    reword more

--- config/conditional-migrations.php
+++ config/conditional-migrations.php
@@ -4,8 +4,8 @@ return [
 
     /*
      * This value will be used to determine whether or not the
-     * conditional migration(s) should run. This an also be
-     * a closure if you need more advanced calculations.
+     * conditional migration(s) should run. This can also be
+     * a closure if you need more complicated calculations.
      */
     'always_run' => false,
 
